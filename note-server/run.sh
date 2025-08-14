e0332aa6dadcc1631845ab0e9042b94591bbdd82
devOp1 <2767908+devOp1@users.noreply.github.com>
2025-08-14 14:50:03 +0200

    run script

---
+++ note-server/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo -k
+echo "Stoppe nginx, mariadb und redis..."
+sudo bash -c "
+        service nginx stop
+        service mariadb stop
+        service redis stop
+"
+
+echo "Starte Laravel Sail..."
+./vendor/bin/sail up
